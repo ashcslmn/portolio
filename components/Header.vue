48cba9e6e790a71e2b3bf2f2c78c0646314e574a
Ashley Solomon <ashcslmn@gmail.com>
2023-03-05 11:17:16 +0800

    pages:(wip) projects fecth error

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 
 <template>
-   <header class="sticky top-0 z-10 w-full px-4 py-4 md:px-0">
+   <header class="sticky top-0 z-10 w-full px-4 py-4 bg-white shadow md:px-0">
         <div id="header-content" class="flex items-center max-w-6xl mx-auto">
             <slot name="logo"/>
             <slot name="nav"/>
